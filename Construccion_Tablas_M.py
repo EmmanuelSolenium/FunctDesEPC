76bc19e0f61275812070ca20872a27892b1a75b3
Emmanuel Florez <emmanuel@solenium.co>
2025-12-10 18:06:53 -0500

    Construccion de tablas

---
+++ Construccion_Tablas_M.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+import math
+
+import pandas as pd
+
+PlanillaCME = pd.read_excel(r"C:\Users\usuario\Documents\PlanillaCMETotal-final.XLS",header=3)
+print(PlanillaCME)
+print(PlanillaEST)
